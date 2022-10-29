4bcdde8af071a666777b0c1611b3417b4362312b
shubhu1026 <shubhu.p2000@gmail.com>
2022-10-30 00:38:54 +0530

    fixed type error

--- src/App.js
+++ src/App.js
@@ -5,24 +5,39 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank';
 import Particles from './components/Particles/Particles';
 import FaceRecognition from './components/FaceRecognition/FaceRecongnition'
-import { Component } from 'react';
-import Clarifai from 'clarifai';
+import React, { Component } from 'react';
 
 const USER_ID = 'shubhu1026';
 // Your PAT (Personal Access Token) can be found in the portal under Authentification
 const PAT = '4e15238e6f6443a88796c2219d3253c9';
 const APP_ID = 'face-it';
-// Change these to whatever model and image URL you want to use
-const MODEL_ID = 'face-detection';
-const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105'; 
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = JSON.parse(data).outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.left_col * width),
+      bottomRow: height - (clarifaiFace.top_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -50,19 +65,19 @@ class App extends Component {
           ]
       });
 
-      const requestOptions = {
-          method: 'POST',
-          headers: {
-              'Accept': 'application/json',
-              'Authorization': 'Key ' + PAT
-          },
-          body: raw
-      };
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
 
-      fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
+      .then(response => response.text())
+      .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
+      .catch(error => console.log('error', error));
   }
 
   render(){
@@ -73,7 +88,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonDetect={ this.onButtonDetect }/>
-        <FaceRecognition imageUrl={ this.state.imageUrl }/>
+        <FaceRecognition box={this.state.box} imageUrl={ this.state.imageUrl }/>
       </div>
     );
   }
